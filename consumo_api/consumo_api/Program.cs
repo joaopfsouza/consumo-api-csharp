68cb5286994ed31b13f0ce29507f8c825482e49b
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-05-07 18:34:52 -0300

    Primeiro teste Newtonsoft/ApiTFS

--- consumo_api/consumo_api/Program.cs
+++ consumo_api/consumo_api/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace consumo_api
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string personalaccesstoken = "srb3k6zh7ahrhkyzmdbcj222f2tm3oaofqfj3xbvjmd6wg77ut3q";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", "", personalaccesstoken))));
+
+                    using (HttpResponseMessage response = await client.GetAsync("http://tfs:8080/tfs/KrotonCollection/_apis/projects"))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(responseBody);
+
+                        IList<string> projectTFS = json.SelectToken("value").ToList().Select(s =>
+                        {
+                            Console.WriteLine(s.SelectToken("name").Value<string>());
+                            return "teste";
+
+                        }).ToList();
+
+                        //foreach (var item in projectTFS)
+                        //{
+                        //    Console.WriteLine(item);
+                        //}
+
+                        //IEnumerable<JToken> 
+
+                        //Console.WriteLine(json.SelectToken());
+                    }
+
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ApiPostTest()
         {
             //Post
             string commentsApi = String.Format("https://jsonplaceholder.typicode.com/posts");
@@ -38,9 +92,6 @@ namespace consumo_api
 
             }
         }
-
-
-
 
         private static void GetTestWebRequest()
         {
